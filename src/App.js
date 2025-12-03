82b31a299ea47563eede0a6ac5b1fad68aa0e740
Upendra Nilupul <139964152+UpendraNilupul@users.noreply.github.com>
2025-12-03 22:06:52 +0530

    Refactor App component layout and styles; add Unit component for data display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Unit from "./Components/Unit";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [imageUrl,setImageUrl] = useState('')
+  const [name,setName] = useState('')
+  const [city,setCity] = useState('')
+  const [position,setPosition] = useState('')
+  
+  const [myData, setMydata] = useState([]);
+
+  //console.log(myData);
+
+  return(<div className="main_container">
+    <div className="main_left">
+      <input type="text"value={imageUrl} onChange={(e)=>{
+        e.preventDefault()
+        setImageUrl(e.target.value)
+      }} placeholder="image url"/>
+      <input type="text" value={name} onChange={(e)=>{
+        e.preventDefault()
+        setName(e.target.value)
+      }} placeholder="Name"/>
+      <input type="text" value={city} onChange={(e)=>{
+        e.preventDefault()
+        setCity(e.target.value)
+      }} placeholder="City"/>
+      <input type="text" value={position} onChange={(e)=>{
+        e.preventDefault()
+        setPosition(e.target.value)
+      }} placeholder="Position"/>
+      <button onClick={()=> 
+      {
+        setMydata((pre)=> [
+          ...pre,{
+            image: imageUrl,
+            name,
+            city,
+            position,
+          },
+        ]);
+
+        setImageUrl((pre)=>{
+          if(pre.length>0) {
+            return ''
+          } else {
+            return pre
+          }
+
+        });
+
+        setName((amila)=> (amila.length) > 0 ? "" : amila);
+        setCity(preCity=> (preCity.length > 0 ? "" : preCity));
+        setPosition((prePosition)=> prePosition.length > 0 ? "" : prePosition);
+        
+      }        
+    }>Submit</button>
+
     </div>
-  );
+
+    <div className="main_right">
+      {myData?.map(({image,name,city,position},index)=>(
+        <Unit 
+          image={image}
+          name={name}
+          city={city}
+          position={position}
+          key={index}
+      />
+      ))}
+    </div>
+  </div>)
 }
 
 export default App;
